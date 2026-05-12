b5b5d22cf64143392f7f874ef0b178626e198228
khan <ziyodullokhan009@mail.ru>
2026-05-12 20:16:00 +0900

    feat: new rejapro

---
+++ app.js
@@ -0,0 +1,18 @@
+// ========= KHAN
+
+console.log("Hello MIT");
+const express = require("express");
+const app = require();
+
+// 1. Krish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ======= KHAN
+
+
+
+// 2. session code
+// 3. views code
+// 4. routing code
