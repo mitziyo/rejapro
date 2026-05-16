f414a2a2541758f1f5e7ae63c60abf3e31d27dc5
Azizbek <azizbekreimbaev1003@gmail.com>
2026-05-16 16:20:36 +0900

    feat: front-end localhost 3000da ishga tushmoqda va views code yozildi darslikda ishlatilgan external package versiayalri tog'rlandi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 console.log("Hello MIT");
 const express = require("express");
-const app = require();
+const app = express();
 
 // 1. Krish code
 app.use(express.static("public"));
@@ -14,5 +14,32 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // 2. session code
+
+
+// ========= Kevin
 // 3. views code
+app.set("views", "views")
+app.set("view engine", "ejs")
+// ========= Kevin
+
 // 4. routing code
+
+
+
+
+
+
+
+
+// ========= Kevin
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.render("reja.ejs")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+// localhost 300 portda ishga tushmoqda front-end
+// ========= Kevin
