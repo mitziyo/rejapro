d38e652078467ad018328b8476eaa0e57184a8d1
khan <ziyodullokhan009@mail.ru>
2026-05-26 17:41:42 +0900

    feat: added server js

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 console.log("Hello MIT42");
+
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://khan:VZasEarecYfVqVHx@cluster0.cgcf8jr.mongodb.net/reja?retryWrites=true&w=majority";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDb");
+    else {
+      console.log("MongoDB connected succesfully");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`,
+        );
+      });
+    }
+  },
+);
